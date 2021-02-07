a689e4ca07096f4f3abde3d3906c5caf68dd0445
niclin <bboyceo@hotmail.com>
2021-02-08 00:24:01 +0800

    Adjustment CSS style use tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import "./index.css";
 
 const App = () => {
   const [url, setUrl] = useState("https://youtu.be/BGaDN9wxbKE");
   const [startText, setStartText] = useState("");
   const [endText, setEndText] = useState("");
   const [comments, setComments] = useState([]);
+  const [replyedCommentId, setReplyedCommentId] = useState([])
 
   const fetchYoutubeComment = () => {
     const videoId = url.split("/").slice(-1)[0];
@@ -17,7 +19,7 @@ const App = () => {
       })
       .then(function (myJson) {
         const comments = myJson.items.map((item) => {
-          return item.snippet.topLevelComment.snippet.textOriginal;
+          return { commentId: item.id, content: item.snippet.topLevelComment.snippet.textOriginal };
         });
 
         const reverseComments = comments.reverse();
@@ -32,7 +34,11 @@ const App = () => {
 
   const fliterComments = () => {
     return comments.filter((comment) => {
-      const lowerCaseComment = comment.toLowerCase();
+      if (replyedCommentId.includes(comment.commentId)) {
+        return
+      }
+
+      const lowerCaseComment = comment.content.toLowerCase();
       if (
         lowerCaseComment.includes(startText) &&
         lowerCaseComment.includes(endText)
@@ -42,45 +48,77 @@ const App = () => {
     });
   };
 
+  const tagIsReplyed = (commentId) => {
+    setReplyedCommentId([...replyedCommentId, commentId])
+  }
+
   return (
-    <div className="App">
-      <form onSubmit={(event) => handelSubmit(event)}>
-        <label>
-          Youtube URL:
-          <input
-            type="text"
-            name="url"
-            value={url}
-            onChange={(event) => setUrl(event.target.value)}
-          />
-        </label>
+    <div className="App" className="container mx-auto">
+      <div className="grid grid-cols-1">
+        <div>
+          <form
+            className="bg-white shadow-md rounded px-8 py-8 pt-8"
+            onSubmit={(event) => handelSubmit(event)}
+          >
+            <label className="text-sm block font-bold pb-2">
+              Youtube URL:
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 "
+                type="text"
+                name="url"
+                value={url}
+                onChange={(event) => setUrl(event.target.value)}
+              />
+            </label>
 
-        <label>
-          開頭文字
-          <input
-            type="text"
-            name="StartText"
-            value={startText}
-            onChange={(event) => setStartText(event.target.value)}
-          />
-        </label>
+            <label className="text-sm block font-bold pb-2">
+              開頭文字
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 "
+                type="text"
+                name="StartText"
+                value={startText}
+                onChange={(event) => setStartText(event.target.value)}
+              />
+            </label>
 
-        <label>
-          結尾文字
-          <input
-            type="text"
-            name="endText"
-            value={endText}
-            onChange={(event) => setEndText(event.target.value)}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-        <ol>
-          {fliterComments().map((comment, index) => {
-            return <li key={index}><p>{comment}</p></li>;
-          })}
-        </ol>
-      </form>
+            <label className="text-sm block font-bold pb-2">
+              結尾文字
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 "
+                type="text"
+                name="endText"
+                value={endText}
+                onChange={(event) => setEndText(event.target.value)}
+              />
+            </label>
+            <input
+              type="submit"
+              value="送出"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            />
+          </form>
+        </div>
+
+        <div>
+          <div className="bg-white shadow-md px-8 py-8 pt-8">
+            <ol className="list-disc">
+              {fliterComments().map((comment) => {
+                return (
+                  <div className="shadow-inner rounded px-8 py-8 pt-8 mb-8 border border-indigo-60">
+
+                    <p className="comment-text">{comment.content}</p>
+                    <button className="mt-8 bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={() => tagIsReplyed(comment.commentId)}>
+                      已回覆
+                    </button>
+
+                  </div>
+                );
+              })}
+            </ol>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
